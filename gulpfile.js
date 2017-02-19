e3364a743c4eeaad6c9fb692cb94fa0adba45d52
Cédric Aellen <cedric.aellen@alienlebarge.ch>
2017-02-19 12:14:37 +0100

    lint json via gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,14 @@
-var gulp = require("gulp")
-var postcss = require("gulp-postcss")
-var reporter = require("postcss-reporter")
-var stylelint = require("stylelint")
+var gulp = require("gulp");
+var postcss = require("gulp-postcss");
+var reporter = require("postcss-reporter");
+var stylelint = require("stylelint");
+var jsonlint = require("gulp-jsonlint");
+
+gulp.task("lint:json", function () {
+  gulp.src("./.stylelintrc")
+      .pipe(jsonlint())
+      .pipe(jsonlint.reporter());
+})
 
 gulp.task("lint:css", function () {
   return gulp.src("test/**/*.css")
